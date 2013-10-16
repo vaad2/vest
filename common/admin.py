c04134db5e4a47ee5cc0b05cdac64b9e0c3c4b97
vaad2@yandex.ru <vaad2@yandex.ru>
2013-10-16 22:17:04 +0400

    add autodiscover for admin

--- common/admin.py
+++ common/admin.py
@@ -1,1 +1,29 @@
-__author__ = 'ld'
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proff.settings")
+
+from django.contrib.admin.sites import AdminSite
+from django.utils.importlib import import_module
+
+import logging
+logger = logging.getLogger('proff')
+
+
+class SuperAdminSite(AdminSite):
+    def has_permission(self, request):
+        return request.user.is_superuser and request.user.is_active and request.user.is_staff
+
+
+def autodiscover(admin_super):
+    from django.conf import settings
+    for app in settings.INSTALLED_APPS:
+        try:
+            module = import_module('.admin', app)
+            try:
+                module.admin_register(admin_super)
+                print 'fn ok'
+            except AttributeError:
+                logger.debug('cant import fn')
+
+        except BaseException, e:
+            logger.debug('cant find admin module %s' % app)
+
