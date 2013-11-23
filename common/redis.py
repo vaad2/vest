49778de31762b95f148ebdb8c06b2e91b35476f0
vaad2 <vaad2@yandex.ru>
2013-11-23 20:34:32 +0400

    fix: convert to class

--- common/redis.py
+++ common/redis.py
@@ -5,27 +5,30 @@ from common.thread_locals import get_thread_var, set_thread_var
 
 logger = logging.getLogger('redis')
 
+class RedisManager(object):
+    def __init__(self, connection = 'redis'):
+        self.connection = connection
 
-def redis_write(key, val):
-    redis_get().set(key, ujson.dumps(val))
-
-
-def redis_read(key, default=None):
-    val = redis_get().get(key)
-    if val is None:
-        return default
-    return ujson.loads(redis_get().get(key))
+    def redis_write(self, key, val):
+        self.redis_get().set(key, ujson.dumps(val))
 
 
-def redis_publish(channel, val):
-    redis_get().publish(channel, ujson.dumps(val))
+    def redis_read(self, key, default=None):
+        val = self.redis_get().get(key)
+        if val is None:
+            return default
+        return ujson.loads(self.redis_get().get(key))
 
 
-def redis_get(connection='redis'):
-    redis = get_thread_var(connection)
-    if not redis:
-        redis = Redis()
-        set_thread_var(connection, redis)
+    def redis_publish(self, channel, val):
+        self.redis_get().publish(channel, ujson.dumps(val))
 
-    return redis
+
+    def redis_get(self):
+        redis = get_thread_var(self.connection)
+        if not redis:
+            redis = Redis()
+            set_thread_var(self.connection, redis)
+
+        return redis
 
