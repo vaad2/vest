2ad8d974d444bbb3ef1243abf45bc5a7667c4045
vaad2 <vaad2@yandex.ru>
2013-12-27 08:03:37 +0400

    fix:payment

--- payment.py
+++ payment.py
@@ -1,3 +1,7 @@
+from copy import deepcopy
+from urllib import urlencode
+
+
 class VTPayment(object):
     def pay_data(self, **kwargs):
         pass
@@ -8,13 +12,97 @@ class VTPayment(object):
     def pay_result(self, **kwargs):
         pass
 
+
+# MrchLogin=sMerchantLogin&
+# OutSum=nOutSum&
+# InvId=nInvId&
+# Desc=sInvDesc&
+# SignatureValue=sSignatureValue
+# IncCurrLabel=sIncCurrLabel&
+# Culture=sCulture
+
 class VTRobokassa(VTPayment):
+    reserved = [
+        'MerchantLogin', 'OutSum', 'InvId',
+        'Desc', 'SignatureValue', 'IncCurrLabel', 'Culture',
+        'MerchantPass1', 'MerchantPass2'
+    ]
+
+    def __init__(self, **kwargs):
+        self.MerchantLogin = kwargs['MerchantLogin']
+        self.MerchantPass1 = kwargs['MerchantPass1']
+        self.MerchantPass2 = kwargs.get('MerchantPass2', '')
+
+        self.url = 'http://test.robokassa.ru/Index.aspx' if kwargs.get(
+            'TestMode') else 'https://auth.robokassa.ru/Merchant/Index.aspx'
+
+    def crc(self, str_data):
+        import hashlib
+
+        md5 = hashlib.md5()
+        md5.update(str_data)
+
+        return md5.hexdigest()
+
+    def params_pack(self, **kwargs):
+        ex_params = []
+        dc_params = {}
+
+        for key, val in kwargs.iteritems():
+            if key.startswith('shp'):
+                ex_params.append('%s=%s' % (key, val))
+                dc_params[key] = val
+
+        if len(ex_params):
+            ex_params.sort()
+
+        return ex_params, dc_params
+
+    def _signature_get(self, password, **kwargs):
+        if 'MerchantLogin' in kwargs:
+            str_sig = '%s:%.2f:%s:%s' % (kwargs['MerchantLogin'], float(kwargs['OutSum']), kwargs['InvId'], password)
+        else:
+            str_sig = '%.2f:%s:%s' % (float(kwargs['OutSum']), kwargs['InvId'], password)
+
+        ex_params, dc_params = self.params_pack(**kwargs)
+        if len(ex_params):
+            str_sig = '%s:%s' % (str_sig, ':'.join(ex_params))
+
+        return str_sig, dc_params
+
+
+    def pay_data(self, **kwargs):
+        str_sig, params = self._signature_get(self.MerchantPass1, MerchantLogin=self.MerchantLogin, **kwargs)
+        params.update({'MrchLogin': self.MerchantLogin, 'OutSum': '%.2f' % kwargs['OutSum'],
+                       'InvId': kwargs['InvId'],
+                       'Desc': kwargs.get('Desc', ''), 'Culture': kwargs.get('Culture', ''),
+                       'SignatureValue': self.crc(str_sig)
+        })
+
+        return '%s?%s' % (self.url, urlencode(params))
+
+    def pay_confirm(self, **kwargs):
+        crc = kwargs['SignatureValue'].upper()
+        str_sig, params = self._signature_get(self.MerchantPass2, **kwargs)
+
+        return crc == self.crc(str_sig).upper()
+
+    def pay_result(self, **kwargs):
+        crc = kwargs['SignatureValue'].upper()
+        str_sig, params = self._signature_get(self.MerchantPass1, **kwargs)
+
+        return crc == self.crc(str_sig).upper()
+
+
+class _old_VTRobokassa(VTPayment):
     def __init__(self, **kwargs):
         self.mrh_login = kwargs['mrh_login']
         self.mrh_pass1 = kwargs['mrh_pass1']
         self.mrh_pass2 = kwargs.get('mrh_pass2', '')
+
         self.ex_params = kwargs.get('ex_params', {})
-        self.url = 'http://test.robokassa.ru/Index.aspx' if kwargs.get('test_mode') else 'https://auth.robokassa.ru/Merchant/Index.aspx'
+        self.url = 'http://test.robokassa.ru/Index.aspx' if kwargs.get(
+            'test_mode') else 'https://auth.robokassa.ru/Merchant/Index.aspx'
 
     def crc(self, str_data):
         import hashlib
@@ -47,10 +135,11 @@ class VTRobokassa(VTPayment):
         if len(ex_params):
             str_data = '%s:%s' % (str_data, ':'.join(ex_params))
 
-        return self.url, {'MrchLogin': self.mrh_login, 'OutSum': '%.2f' % kwargs['out_sum'], 'InvId': kwargs['inv_id'],
-                          'Desc': kwargs.get('desc', ''), 'SignatureValue': self.crc(str_data)
+        return '%s?%s' % self.url, urlencode(
+            {'MrchLogin': self.mrh_login, 'OutSum': '%.2f' % kwargs['out_sum'], 'InvId': kwargs['inv_id'],
+             'Desc': kwargs.get('desc', ''), 'Culture': kwargs.get('culture', ''),
+             'SignatureValue': self.crc(str_data)})
 
-        }
 
     # MrchLogin=$mrh_login&".
     # "OutSum=$out_summ&InvId=$inv_id&Desc=$inv_desc&SignatureValue=$crc"
@@ -84,7 +173,7 @@ class VTRobokassa(VTPayment):
         # 3d915f8fe1fa7e6eac672076590538e3
 
 
-# robo = VTRobokassa(mrh_login='mirbuketa', mrh_pass1='testtest1', mrh_pass2='testtest2',
-#                    ex_params={'email': 'vaad2@ya.ru'})
-# print robo.pay_data(out_sum=100.00, inv_id=1)
-# print robo.pay_confirm(crc='22588FEC79367B8AE8A99C049531E3BC', out_sum=100.00, inv_id=1)
+        # robo = VTRobokassa(mrh_login='mirbuketa', mrh_pass1='testtest1', mrh_pass2='testtest2',
+        #                    ex_params={'email': 'vaad2@ya.ru'})
+        # print robo.pay_data(out_sum=100.00, inv_id=1)
+        # print robo.pay_confirm(crc='22588FEC79367B8AE8A99C049531E3BC', out_sum=100.00, inv_id=1)
